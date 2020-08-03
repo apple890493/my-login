8f4e99afe71215bd5e0459167211ea0124c1a983
nonoLin <denkanono@gmail.com>
2020-08-03 10:15:28 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-"# my-login" 
+# 登入介面
+以Express & Node.js打造的登入介面
+[!img](https://github.com/apple890493/my-login/blob/master/2-3A10.JPG)
+
+## 功能列表
+- 篩選是否信箱及密碼與資料一致
+
+### 啟動方式
+- 將專案clone到本地端
+  `https://github.com/apple890493/my-login.git`
+- 進入專案
+  `cd my-login`
+- 下載package
+  `npm install`
+- 透過nodemon啟動專案
+  `npm run dev`
+- 最後在terminal可以看到 localhost : 3000
+  `開啟瀏覽器在網址列輸入localhost:3000`
+
+### 開發環境
+- Node.js: v10.15.0
+- Express: v4.17.1
+- Express-Handlebars: 5.1.0
+- Body-parser": 1.19.0
--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session') //引入 express-session
 const matchUser = require('./login')
 
 const app = express()
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+//設置session的參數
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 30 * 1000 }
+}))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  if (req.session.userName) {
+    const result = req.session.userName
+    res.render('workit', { result })
+  } else {
+    res.render('index')
+  }
 })
 
 app.post('/', (req, res) => {
@@ -20,8 +34,14 @@ app.post('/', (req, res) => {
   if (!result) {
     res.render('index', { alert: true })
   } else {
+    req.session.userName = result
     res.render('workit', { result, userInfo })
   }
+})
+
+app.post('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/');
 })
 
 app.listen(3000, () => {
